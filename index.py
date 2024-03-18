c0903c3c0aaaecc02b5809e122e6eb8c29bf3d5d
Everton Espedito <152447787+EvertonEspedito@users.noreply.github.com>
2024-03-18 15:21:20 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,28 @@
-def inverter_string(s):
-    # Convertendo a string para uma lista de caracteres
-    caracteres = list(s)
-    
-    # Invertendo a lista de caracteres
-    tamanho = len(caracteres)
-    for i in range(tamanho // 2):
-        caracteres[i], caracteres[tamanho - i - 1] = caracteres[tamanho - i - 1], caracteres[i]
-    
-    # Construindo a string invertida a partir da lista de caracteres
-    string_invertida = ''.join(caracteres)
-    
-    return string_invertida
+from time import sleep
+from instabot import Bot
 
-# Exemplo de uso
-string_original = "Exemplo"
-string_invertida = inverter_string(string_original)
-print("String original:", string_original)
-print("String invertida:", string_invertida)
+# Crie uma instância do bot
+bot = Bot()
 
+# Faça login na sua conta do Instagram
+bot.login(username=input("Digite o seu Nome de Usuario: "), password=input("Digite Sua Senha: ")+"\n")
+
+# Peça o username do concorrente
+concorrente_username = input("Digite o username do concorrente: ")
+
+# Obtenha os seguidores do concorrente
+seguidores = bot.get_user_followers(concorrente_username)
+
+# Itere sobre os seguidores e curta as 2 últimas fotos de cada um
+for seguidor in seguidores:
+    # Obtenha as 2 últimas fotos do seguidor
+    fotos = bot.get_user_medias(seguidor, filtration=None)
+    for foto in fotos[:2]:  # Pegue apenas as 2 últimas fotos
+        # Curta a foto
+        bot.like(foto)
+        print(f"Fotografia curtida para {seguidor}")
+        # Intervalo para não sobrecarregar o servidor do Instagram e evitar bloqueios
+        sleep(10)  # Intervalo de 10 segundos entre cada curtida
+
+# Logout do bot
+bot.logout()
